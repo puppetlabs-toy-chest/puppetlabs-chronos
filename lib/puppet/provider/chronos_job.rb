1fbc8ce2529fecb0922ca03bef5faa21c01a65f3
Eric Zounes <eric.zounes@puppetlabs.com>
2015-03-26 13:46:05 -0700

    Add some error handling around connecting to the Chronos host.

--- lib/puppet/provider/chronos_job.rb
+++ lib/puppet/provider/chronos_job.rb
@@ -1,31 +1,39 @@
 require 'httparty'
 Puppet::Type.type(:chronos_job).provide(:default) do
 
-    desc "Implements creating Chronos jobs through its REST api."
+  desc "Implements creating Chronos jobs through its REST api."
 
-    def create
-        @job = {
-        'name': resource[:name],
-        'async' : resource[:async],
-        'command': resource[:command],
-        'epsilon': resource[:epsilon],
-        'owner': resource[:owner],
-        }
-        if resource[:parents] == nil
-            @job_type_endpoint = "scheduler/iso8601"
-            job[:schedule] = resource[:schedule]
-            if resource[:schedule_timezone]
-                job[:scheduleTimeZone] = resource[:schedule_timezone]
-            end
-        else
-            @job_type_endpoint = "scheduler/dependency"
-            job[:parents] = resource[:parents]
-        end
-        HTTParty.post("#{resource[:host]}/#{job_type}", job)
+  def create
+    @job = {
+    'name': resource[:name],
+    'async' : resource[:async],
+    'command': resource[:command],
+    'epsilon': resource[:epsilon],
+    'owner': resource[:owner],
+    }
+    if resource[:parents] == nil
+      @job_type_endpoint = "scheduler/iso8601"
+      job[:schedule] = resource[:schedule]
+      if resource[:schedule_timezone]
+        job[:scheduleTimeZone] = resource[:schedule_timezone]
+      end
+    else
+      @job_type_endpoint = "scheduler/dependency"
+      job[:parents] = resource[:parents]
     end
 
-    def destroy
-        HTTParty.delete("#{resource[:host]}/job/#{resource[:name]}")
+    begin
+      response = HTTParty.post("#{resource[:host]}/#{job_type}", job)
+    rescue HTTParty::Error
+      raise Puppet::Error, "Error while connecting to Chronos host #{resource[:host]}"
     end
+  end
 
+  def destroy
+    begin
+      response = HTTParty.delete("#{resource[:host]}/job/#{resource[:name]}")
+    rescue HTTParty::Error
+      raise Puppet::Error, "Error while connecting to Chronos host #{resource[:host]}"
+    end
+  end
 end
