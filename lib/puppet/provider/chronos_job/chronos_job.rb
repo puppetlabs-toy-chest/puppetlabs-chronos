b4414c9166d0c958db95765cef928ba03e2cf6aa
Cody Herriges <cody@herriges.org>
2015-04-02 10:26:04 -0700

    Implement prefetching and instances.
      This commit adds self.instances and self.prefetch methods so that
      "pupept resources chronos_job" works and @property_hash is available
      for.
    
      Without this we have pull ALL jobs from chronos everytime we want to
      check the existance of a single job.
    
      ...also fixes tabs to be Ruby two instead of Python four.

--- lib/puppet/provider/chronos_job/chronos_job.rb
+++ lib/puppet/provider/chronos_job/chronos_job.rb
@@ -1,21 +1,87 @@
-require 'httparty'
-require 'json'
 Puppet::Type.type(:chronos_job).provide(:default) do
+
+  # I've had issues with lazy loading of providers in the past where requirements
+  # for the provider to function are best defined inside the provider as opposed
+  # to the standard Ruby practice.  Allows for you to be able to install httparty
+  # on the same run as the provider is synced.
+  require 'httparty'
+  require 'json'
 
   desc "Implements creating Chronos jobs through its REST api."
 
+  mk_resource_methods
+
+  @default_target = 'http://localhost:4400'
+
+  class << self
+    attr_accessor :default_target
+  end
+
+  def self.targets(resources = nil)
+    targets = []
+    # First get the default target
+    targets << self.default_target
+
+    if resources
+      resources.each do |name, resource|
+        if value = resource[:host]
+          targets << value
+        end
+      end
+    end
+
+    targets.uniq.compact
+  end
+
+  def self.instances(resources = nil)
+
+    instances = []
+
+    ltargets = targets(resources)
+
+    ltargets.each do |target|
+      jobs = HTTParty.get("#{target}/scheduler/jobs")
+
+      jobs.each do |job|
+        job = {
+          :name                  => job['name'],
+          :async                 => job['async'],
+          :command               => job['command'],
+          :environment_variables => jog['environmentVariables'],
+          :epsilon               => job['epsilon'],
+          :owner                 => job['owner'],
+          :retries               => job['retries'],
+          :cpus                  => job['cpus'],
+          :disk                  => job['disk'],
+          :mem                   => job['mem']
+        }
+
+        instances << new(job)
+      end
+    end
+    instances
+  end
+
+  def self.prefetch(resources)
+    instances(resources).each do |prov|
+      if res = resources[prov.name.to_s]
+        res.provider = prov
+      end
+    end
+  end
+
   def create
     job = {
-    "name" => resource[:name],
-    "async" =>  resource[:async],
-    "command" => resource[:command],
-    "environmentVariables" => resource[:environment_variables],
-    "epsilon" => resource[:epsilon],
-    "owner" => resource[:owner],
-    "retries" => resource[:retries],
-    "cpus" => resource[:cpus],
-    "disk" => resource[:disk],
-    "mem" => resource[:mem],
+      'name'                 => resource[:name],
+      'async'                => resource[:async],
+      'command'              => resource[:command],
+      'environmentVariables' => resource[:environment_variables],
+      'epsilon'              => resource[:epsilon],
+      'owner'                => resource[:owner],
+      'retries'              => resource[:retries],
+      'cpus'                 => resource[:cpus],
+      'disk'                 => resource[:disk],
+      'mem'                  => resource[:mem],
     }
 
     headers = {
@@ -42,18 +108,7 @@ Puppet::Type.type(:chronos_job).provide(:default) do
   end
 
   def exists?
-    begin
-      response = HTTParty.get("#{resource[:host]}/scheduler/jobs")
-      body = JSON.parse(response.body)
-      body.each do |job|
-        if resource[:name] == job['name']
-          return true
-        end
-      end
-      return false
-    rescue HTTParty::Error
-      raise Puppet::Error, "Error while connecting to Chronos host #{resource[:host]}"
-    end
+    !(@property_hash[:ensure] == :absent or @property_hash.empty?)
   end
 
   def destroy
