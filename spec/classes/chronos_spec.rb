37054b7c18547ef123fdaf72f976ba874ad39e98
Branden Smith <bsmith@liaison.com>
2016-03-24 17:22:48 -0400

    Correct tests for force_provider (both sys-default and override)

--- spec/classes/chronos_spec.rb
+++ spec/classes/chronos_spec.rb
@@ -24,7 +24,7 @@ describe 'chronos' do
 
         context 'with system-default service provider' do
           it { is_expected.to contain_service('chronos').with({
-            'provider' => 'systemd'
+            'provider' => nil
           }) }
         end
 
@@ -34,7 +34,7 @@ describe 'chronos' do
           end
 
           it { is_expected.to contain_service('chronos').with({
-            'provider' => 'systemd'
+            'provider' => 'upstart'
           }) }
         end
 
